0b1964da3b58135dcad9fd74a1bf9f5835a8c1d6
AzertEM <43853717+AzertEM@users.noreply.github.com>
2019-07-29 15:36:14 +0700

    Update PBCSEQEasy.cpp

--- PBCSEQEasy.cpp
+++ PBCSEQEasy.cpp
@@ -1,33 +1,50 @@
 #include <bits/stdc++.h>
+#define X first
+#define Y second
 
 using namespace std;
-const int Max=1000000;
-int l[Max+1], f[Max+1], n, r[Max+1];
 
-void sapxep() {
-    for (int i=1; i<=n-1; i++)
-        for (int j=i+1; j<=n; j++)
-            if (r[i]-l[i]<r[i]-r[j]) {
-                swap(r[i],r[j]);
-                swap(l[i], l[j]);
-            }
+const int N = 1e5 + 3;
+int n;
+int tail[N];
+pair <int, int> a[N];
+
+int BS(int tail[], int l, int r, int target) {
+    if (r - l == 1) {
+        return r;
+    }
+
+    int mid = (l + r) / 2;
+    if (tail[mid] > target) return BS(tail, l, mid, target);
+    return BS(tail, mid, r, target);
 }
 
 int main() {
+    // freopen("PBCSEQ.inp", "r", stdin);
+    // freopen("PBCSEQ.out", "w", stdout);
     scanf("%d", &n);
-    for (int i=1; i<=n; i++) {
-        scanf("%d%d", &l[i], &r[i]);
-        f[i]=1;
+    for (int i = 1; i <= n; i++) {
+        scanf("%d%d", &a[i].X, &a[i].Y); 
+        a[i].X = -a[i].X;
     }
-    sapxep();
-    for (int i=1; i<=n-1; i++) {
-        for (int j=i+1; j<=n; j++) {
-            if (l[j]>=l[i] && r[j]<=r[j])
-                f[j]=max(f[i]+1, f[j]);
+
+    sort(a + 1, a + n + 1);
+    for (int i = 1; i <= n; i++) a[i].X = -a[i].X;
+
+    int res = 1;
+    tail[1] = a[1].Y;
+    for (int i = 2; i <= n; i++) {
+        if (a[i].Y < tail[1])
+            tail[1] = a[i].Y;
+        
+        else if (a[i].Y >= tail[res])
+            tail[++res] = a[i].Y;
+
+        else {
+            int k = BS(tail, 0, res, a[i].Y);
+            tail[k] = a[i].Y;
         }
+
     }
-    int res=0;
-    for (int i=1; i<=n; i++)
-        res=max(res, f[i]);
     printf("%d", res);
 }
